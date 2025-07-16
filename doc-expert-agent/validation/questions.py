39a18e0a11572d15769f61e005ec986f0494f0a8
Thales Marega <thalesmarega@Thaless-MacBook-Pro.local>
2025-07-15 22:17:30 -0300

    format code

---
+++ doc-expert-agent/validation/questions.py
@@ -0,0 +1,26 @@
+avaliacao = [
+    {
+        "pergunta": "quem escreveu o projeto?",
+        "resposta_ideal": "Marlon Iwanaga Pacheco, Rafael Cinquini, Raphael Farias Utida, Thales Bertolini Marega"
+    },
+    {
+        "pergunta": "qual o titulo do trabalho?",
+        "resposta_ideal": "O título do trabalho é SISTEMAS MÓVEIS NO MUNDO ACADÊMICO E SUA IMPORTÂNCIA AO DESENVOLVIMENTO COMPUTACIONAL"
+    },
+    {
+        "pergunta": "em que ano foi escrito?",
+        "resposta_ideal": "O TCC foi escrito em 2014."
+    },
+    {
+        "pergunta": "qual o nome do aplicativo desenvolvido?",
+        "resposta_ideal": "Calculadora de Fisica"
+    },
+    {
+        "pergunta": "qual linguagem o aplicativo foi escrito?",
+        "resposta_ideal": "O aplicativo foi escrito utilizando principalmente as linguagens JavaScript e HTML."
+    },
+    {
+        "pergunta": "qual o objetivo do projeto?",
+        "resposta_ideal": "O objetivo é facilitar a adaptação e integração entre as principais plataformas móveis, Android e iOS, e analisar o impacto dessa aplicação no contexto educacional, proporcionando um método alternativo de aprendizado interativo e envolvente."
+    },
+]
